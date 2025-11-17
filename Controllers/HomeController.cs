820806a1bae702f55abe9cf8d4c252ad4eb9938d
BAC213213 <163724896+BAC213213@users.noreply.github.com>
2025-11-17 20:11:02 +0000

    added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using team_golden_virus.Models;
 
@@ -6,13 +5,6 @@ namespace team_golden_virus.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
@@ -21,12 +13,6 @@ namespace team_golden_virus.Controllers
         public IActionResult Privacy()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
---
+++ ViewsModel/PatientDashboardViewModel.cs
@@ -0,0 +1,49 @@
+﻿using team_golden_virus.Models; // use the project's Models namespace
+using System.Collections.Generic;
+using System;
+
+namespace team_golden_virus.ViewModels
+{
+    // A single data point for the pressure chart
+    public class PressureDataPoint
+    {
+        public string Time { get; set; }
+        public double Pressure { get; set; }
+        public double Threshold { get; set; }
+    }
+
+    // A class for the simplified heatmap zones
+    public class HeatmapZone
+    {
+        public string Label { get; set; }
+        public int Pressure { get; set; }
+    }
+
+    // The main ViewModel to hold all data for the dashboard
+    public class PatientDashboardViewModel
+    {
+        // Patient Info (from Header)
+        public string PatientName { get; set; }
+        public string PatientId { get; set; }
+
+        // Clinician Info (from DoctorContact)
+        public string ClinicianName { get; set; }
+        public string ClinicianSpecialty { get; set; }
+        public string ClinicianEmail { get; set; }
+        public DateTime NextAppointment { get; set; }
+
+        // Alerts (from AlertsPanel)
+        public List<Alert> ActiveAlerts { get; set; } // 'Alert' now defined in Models
+
+        // Pressure Chart (from PressureChart)
+        public List<PressureDataPoint> PressureHistory { get; set; }
+
+        // Heatmap (from FootHeatmap)
+        // We can use the simplified zones from your React file
+        public List<HeatmapZone> HeatmapZones { get; set; }
+
+        // --- OR ---
+        // We can use the 32x32 grid from your project brief
+        // public int[,] PressureGrid { get; set; } 
+    }
+}
